f22f14942fb0619b5a177a0614ab9278ea1fb5e6
TranVuHoang <tranvuhoangjr@gmail.com>
2025-03-01 08:43:13 +0700

    create bai2

---
+++ bai2/bai2/bai2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    //Xuất dữ liệu hiển thị ra màn hình
+    cout << "Chuong trinh c++ dau tien\n";
+
+    //Nhập dữ liệu từ bàn phím
+    float math_score, physical_score, chemistry_score, mean_score;
+    cout<< "Moi ban nhap vao diem toan:";
+    // Nhập
+    cin >> math_score;  
+
+    cout << "Moi ban nhap vao diem ly:";    
+    // Nhập
+    cin >> physical_score;
+
+    cout << "Moi ban nhap vao diem hoa:";
+    // Nhập
+    cin >> chemistry_score;
+
+    //Tính điểm trung bình
+    cout << "Diem trung binh 3 mon hoc: " << (math_score + physical_score + chemistry_score) / 3;
+
+}
